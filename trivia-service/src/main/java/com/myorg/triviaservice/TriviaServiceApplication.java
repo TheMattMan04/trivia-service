b19f160842fd32f85831fa82a7be9a0a4e030ed7
Matthew Augustine <matt@matthews-air.kpn>
2022-07-21 13:04:12 +0200

    initial commit

---
+++ trivia-service/src/main/java/com/myorg/triviaservice/TriviaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myorg.triviaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaServiceApplication.class, args);
+	}
+
+}
---
+++ trivia-service/src/test/java/com/myorg/triviaservice/TriviaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myorg.triviaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
